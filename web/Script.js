cdf06df3a76d0709a1170b3bf1bd88d6099153af
I056836 <clement.gailly.de.taurines@sap.com>
2018-02-15 13:34:49 +0100

    Update web site

--- web/Script.js
+++ web/Script.js
@@ -1,40 +1,35 @@
+$(document).ready(function () {
+    var timer = null;
 
-$(document).ready(function() {
- 
+    function sendMotorCommand(cmd) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", '/motor/' + cmd, true);
+        xhttp.send();
+    }
 
+    function addButtonEventListners(btnId, command) {
+        btn = document.getElementById(btnId);
+        btn.addEventListener("mousedown", function (evt) {
+            timer = setInterval(function () {
+                console.log(command);
+                sendMotorCommand('left');
+            }, 500);
+        });
+        btn.addEventListener("mouseup", function (evt) {
+            clearInterval(timer);
+            sendMotorCommand('stop');
+            console.log("Stop!");
+        });
+        btn.addEventListener("mouseleave", function (evt) {
+            clearInterval(timer);
+            sendMotorCommand('stop');
+            console.log("Stop!");
+        });
 
-main = document.getElementById("btnFwd");
-
-// Variable to hold a reference to the timer
-var timer = null;
-
-// Set up an event handler for mousedown
-main.addEventListener("mousedown", function(evt){
-  // Start a timer that fires a function at 50 millisecond intervals
-  timer = setInterval(function(){
-    // the function can do whatever you need it to
-    console.log("Mous is down!");
-  }, 50);
-});
-
-
-// Set up a custom mouseup event handler
-main.addEventListener("mouseup", function(evt){
-  // Cancel the previously initiated timer function
-  clearInterval(timer);
-  
-  // And, do w0+hatever else you need to:
-  console.log("Mouse is Up!");
-});
-
-// Set up a custom mouseleave event handler so that
-// if the mouse is dragged out of the original element
-// and then the mouse is released, the timer will stop
-main.addEventListener("mouseleave", function(evt){
-  // Cancel the previously initiated timer function
-  clearInterval(timer);
-  
-  // And, do whatever else you need to:
-  console.log("Mouse is no longer on element!");
-});
+    }
+    addButtonEventListners('btnFwd', "forward");
+    addButtonEventListners('btnLeft', "left");
+    addButtonEventListners('btnRight', "right");
+    addButtonEventListners('btnStop', "stop");
+    addButtonEventListners('btnDown', "backward");
 });
